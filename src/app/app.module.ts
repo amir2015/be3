06f7a7119ea1d7b9a7b9fea5a2004f906b161f16
amir2015 <zahranovich2@gmail.com>
2019-02-09 01:40:13 -1000

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserformComponent } from './userform/userform.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserformComponent,
+    UserloginComponent
   ],
   imports: [
     BrowserModule,
